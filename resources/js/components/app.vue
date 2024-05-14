d63eb93dadbe2ac446e1001f9764fbd426636a98
Primi <angel-primi@hotmail.com>
2024-05-14 17:58:13 -0600

    chore: Update navigation bar in app.vue component

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,11 +1,13 @@
 <template>
-    <h1>Hello App!</h1>
-    <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
-    <nav>
-        <RouterLink to="/">Go to Home</RouterLink>
-        <RouterLink to="/login">Go to login</RouterLink>
+    <nav class="bg-gray-800 p-4 text-white flex justify-between">
+        <RouterLink to="/">Home</RouterLink>
+        <!-- <p><strong>Current route path:</strong> {{ $route.fullPath }}</p> -->
+        <div class="flex space-x-4">
+            <RouterLink to="/signup">Sign Up</RouterLink>
+            <RouterLink to="/login">Log in</RouterLink>
+        </div>
     </nav>
-    <main>
+    <main class="bg-gray-100 p-4 min-h-[calc(100vh-64px)]">
         <RouterView />
     </main>
 </template>
