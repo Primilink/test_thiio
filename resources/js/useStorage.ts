01146d8e38607125281aa2d9f12343eb40b3d601
Primi <angel-primi@hotmail.com>
2024-05-14 18:05:05 -0600

    chore: Refactor useStorage to log key actions

--- resources/js/useStorage.ts
+++ resources/js/useStorage.ts
@@ -1,14 +1,17 @@
 const useStorage = () => {
     const setItem = (key: string, value: any) => {
+        console.log("setItem", key, value);
         localStorage.setItem(key, JSON.stringify(value));
     };
 
     const getItem = (key: string) => {
+        console.log("getItem", key);
         const value = localStorage.getItem(key);
         return value ? JSON.parse(value) : null;
     };
 
     const removeItem = (key: string) => {
+        console.log("removeItem", key);
         localStorage.removeItem(key);
     };
 
